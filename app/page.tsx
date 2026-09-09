16ca4cbc9b17d8503db57ccefcbc21a12e16f802
Max Chmelicek <max.chmelicek@gmail.com>
2026-09-09 12:40:38 +0200

    project: edited some config

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ const page = () => {
   return (
     <div className='w-full h-full min-h-screen bg-radial from-gray-800 to-black flex flex-col justify-center items-center'>
       <div className='flex flex-col justify-center items-start gap-2'>
-        <h1 className='text-white font-light text-4xl'>Pomodoro Timer</h1>
+        <h1 className='text-white font-light text-5xl'>Pomodoro Timer</h1>
         <PomoTimer />
       </div>
     </div>
